bff8309a07661e8e5ba90e1f13eda470c5fdd8fc
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2019-12-26 23:13:51 +0100

    Write tests for server.properties

--- build/resources/test/ServerPropsTest.kt
+++ build/resources/test/ServerPropsTest.kt
@@ -1,7 +1,10 @@
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import java.io.File
 import java.io.FileInputStream
+import java.nio.file.Files
+import java.nio.file.Path
 import java.util.*
 
 class ServerPropsTest {
@@ -10,13 +13,13 @@ class ServerPropsTest {
 
     init {
         FileInputStream("src/main/resources/server.properties").use { input ->
-            prop.load(input)
+            prop.load(input);
         }
     }
 
     @Test
     fun `check server properties exist`() {
-        val file = File("src/main/resources/server.properties")
+        val file: File = File("src/main/resources/server.properties")
         val exists = file.exists()
         Assertions.assertEquals(true, exists)
     }
