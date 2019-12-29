a8a43c0c2ada7ef0015363ee8d0b1dd305722422
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2019-12-29 20:55:51 +0100

    Add tests userHandlingTest functions chekcHowManyUsers createUsers deleteUsers

--- src/main/kotlin/user/UserHandling.kt
+++ src/main/kotlin/user/UserHandling.kt
@@ -3,25 +3,28 @@ package user
 import database.DatabaseConnection
 import java.sql.ResultSet
 
-class UserHandling {
+class UserHandling (){
+
+    private val statement = DatabaseConnection.connection.createStatement()
 
     fun createUser(name: String, surname: String, mail: String, isAdmin: Boolean) {
-        val statement = DatabaseConnection.connection.createStatement()
-        val sqlSelect = "SELECT count(\"id\") from public.users;"
-        val howManyColumn :  ResultSet = statement.executeQuery(sqlSelect)
-        var id : Int = 0;
-        while (howManyColumn.next()){
-            id = howManyColumn.getInt("id")
-        }
-        val sqlInsert = "INSERT INTO public.users values (${id.toString()}, '${surname}', '${name}', '${mail}', ${isAdmin}, false,'activation code');"
-        statement.executeQuery(sqlInsert)
-        statement.close()
+        val id = howManyUsersExist()
+        val sqlInsert = "INSERT INTO public.users values (${(id+1).toString()}, '${surname}', '${name}', '${mail}', ${isAdmin}, false,'activation code');"
+        statement.executeUpdate(sqlInsert)
     }
 
     fun deleteUser(id: Int){
         val statement = DatabaseConnection.connection.createStatement()
         val sqlDelete = "DELETE FROM public.users WHERE id=${id}"
-        statement.executeQuery(sqlDelete)
-        statement.close()
+        statement.executeUpdate(sqlDelete)
+    }
+
+    fun howManyUsersExist (): Int {
+        val sqlSelect = "SELECT count(\"id\") from public.users;"
+        val howManyColumn :  ResultSet = statement.executeQuery(sqlSelect)
+        while (howManyColumn.next()){
+            return howManyColumn.getInt("count")
+        }
+        return 0
     }
 }
--- src/test/kotlin/UserHandlingTest.kt
+++ src/test/kotlin/UserHandlingTest.kt
@@ -1,7 +1,24 @@
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import user.UserHandling
 
 class UserHandlingTest {
 
-    val userHandling = UserHandling();
+    private val userHandling = UserHandling();
+
+    @Test
+    fun `check fun how many user exist`(){
+        Assertions.assertEquals(true, userHandling.howManyUsersExist()>=0)
+    }
+
+    @Test
+    fun `insert new user to database`(){
+        userHandling.createUser("Test", "Test", "test@test.com", true)
+    }
+
+    @Test
+    fun `delete new user`(){
+        userHandling.deleteUser(userHandling.howManyUsersExist())
+    }
+
 }
