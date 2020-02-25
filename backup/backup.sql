8e73f20d7af10aa92086cd9ad98aed730fecffaf
Mateusz Karłowski <mateusz.karlowski@zoho.com>
2020-02-25 11:45:33 +0100

    Add datatbase backup

--- backup/backup.sql
+++ backup/backup.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 11.2 (Debian 11.2-1.pgdg90+1)
--- Dumped by pg_dump version 11.2 (Debian 11.2-1.pgdg90+1)
+-- Dumped from database version 12.1 (Debian 12.1-1.pgdg100+1)
+-- Dumped by pg_dump version 12.1 (Debian 12.1-1.pgdg100+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -12,12 +12,25 @@ SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
+SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
 SET default_tablespace = '';
 
-SET default_with_oids = false;
+SET default_table_access_method = heap;
+
+--
+-- Name: session; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.session (
+    id character varying NOT NULL,
+    user_id integer
+);
+
+
+ALTER TABLE public.session OWNER TO postgres;
 
 --
 -- Name: users; Type: TABLE; Schema: public; Owner: postgres
@@ -38,26 +51,37 @@ CREATE TABLE public.users (
 ALTER TABLE public.users OWNER TO postgres;
 
 --
--- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: session; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.users (id, surname, name, mail, is_admin, is_active, verification_key, password) FROM stdin;
+COPY public.session (id, user_id) FROM stdin;
+r�@~G\v	1
+df62e2bba1f168e23d49fd327d4b386b92ee6e031f9db554847070cc3a5975bb	0
 \.
 
 
 --
--- Name: users users_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_pk PRIMARY KEY (id);
+COPY public.users (id, surname, name, mail, is_admin, is_active, verification_key, password) FROM stdin;
+0	Ktos	mine	nsf@hfjs.gmail	\N	t	haslo	haslo
+\.
 
 
 --
--- Name: users_mail_uindex; Type: INDEX; Schema: public; Owner: postgres
+-- Name: session session_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-CREATE UNIQUE INDEX users_mail_uindex ON public.users USING btree (mail);
+ALTER TABLE ONLY public.session
+    ADD CONSTRAINT session_pk PRIMARY KEY (id);
+
+
+--
+-- Name: session_id_uindex; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE UNIQUE INDEX session_id_uindex ON public.session USING btree (id);
 
 
 --
